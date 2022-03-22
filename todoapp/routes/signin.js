0ce72f0e104f873d82d4309d0e01d266c46fd876
kitsuya0828 <kitsuyaazuma@gmail.com>
2022-03-22 17:52:35 +0900

    finished chapter 17

--- todoapp/routes/signin.js
+++ todoapp/routes/signin.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const knex = require('../db/knex');
+const bcrypt = require('bcrypt');
 
 router.get('/', function (req, res, next) {
     const userId = req.session.userid;
@@ -21,20 +22,26 @@ router.post('/', function (req, res, next) {
     knex('users')
         .where({
             name: username,
-            password: password,
         })
         .select('*')
-        .then((results) => {
-            // console.log(resluts);
+        .then(async function(results) {
+            // const comparedPassword = await bcrypt.compare(password, results[0].password);
+            // console.log(comparedPassword);
             if (results.length === 0) {
                 res.render('signin', {
                     title: "Sign in",
                     errorMessage: ["ユーザーが見つかりません"],
                     isAuth: isAuth,
                 });
-            } else {
+            } else if (await bcrypt.compare(password, results[0].password)) {
                 req.session.userid = results[0].id;
                 res.redirect('/');
+            } else {
+                res.render('signin', {
+                    title: "Signin", 
+                    errorMessage: ["ユーザが見つかりません"],
+                    isAuth: isAuth,
+                });
             }
         })
         .catch(function (err) {
